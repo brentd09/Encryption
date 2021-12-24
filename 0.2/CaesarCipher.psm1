3bb020ac43a1b8f9c43e651d55b03b1a2df10559
Brent Denny <brent.denny@ddls.com.au>
2021-12-24 23:36:04 +1000

    added the ability to shift by number

---
+++ 0.2/CaesarCipher.psm1
@@ -0,0 +1,87 @@
+﻿function ConvertTo-CaesarCipher {
+  <#
+  .SYNOPSIS
+    Converts alphabet characters to a shifted cipher
+  .DESCRIPTION
+    Converts alphabet characters in a string to a Caesar Cipher 
+    which simply shifts the characters forward up the alphabet.
+    If its runs out of alphabet z wraps arond to a and it starts 
+    from there
+  .EXAMPLE
+    ConvertTo-CaesarCipher -Text 'The quick brown fox jumps over the lazy dog' -Shift 3
+    This will encrypt the string so that it is no longer human readable 
+  .PARAMETER Text
+    The text that will be obscured via the Caesar Cipher 
+  .PARAMETER Shift
+    How many characters to shift the text to produce the cipher 
+  .INPUTS  
+    String
+  .OUTPUTS
+    String
+  .NOTES
+    General notes
+      Created by: Brent Denny
+      Created on: 25 Dec 2021
+      Versions: 
+  #>
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Text,
+    [int]$Shift = 1
+  )
+  $CipherResult = ''
+  $Chars = $Text.ToCharArray()
+  foreach ($Char in $Chars) {
+    $Ascii = [byte][char]$Char
+    if ($Char -match '[a-z]') {
+      if (($Ascii -ge (123-$Shift) -and ($Ascii -le 122)) -or ($Ascii -ge (91-$Shift) -and ($Ascii -le 90))) {$Ascii = $Ascii - (26-$Shift)}
+      else {$Ascii = $Ascii + $Shift}
+    }
+    $CipherResult += [char]$Ascii
+  }
+  return $CipherResult
+}
+
+function ConvertFrom-CaesarCipher {
+    <#
+  .SYNOPSIS
+    Converts a shifted cipher back into readable text
+  .DESCRIPTION
+    Converts a Caesar Cipher back into readable text which shifts the characters 
+    back a chosen number of characters in the alphabet.
+  .EXAMPLE
+    ConvertFrom-CaesarCipher -Cipher 'Wkh txlfn eurzq ira mxpsv ryhu wkh odcb grj' -Shift 3
+    This will decrypt the cipher so that it is now human readable given the correct shift value
+  .PARAMETER Cipher
+    The Cipher that will be unobscured, reversing the Caesar Cipher 
+  .PARAMETER Shift
+    How many characters to shift alphabet of the cipher  
+  .INPUTS  
+    String
+  .OUTPUTS
+    String
+  .NOTES
+    General notes
+      Created by: Brent Denny
+      Created on: 25 Dec 2021
+      Versions: 
+  #>
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Cipher,
+    [int]$Shift = 1
+  )
+  $TextResult = ''
+  $Chars = $Cipher.ToCharArray()
+  foreach ($Char in $Chars) {
+    $Ascii = [byte][char]$Char
+    if ($Char -match '[a-z]') {
+      if (($Ascii -le (96+$Shift) -and ($Ascii -ge 97)) -or ($Ascii -le (24+$Shift) -and ($Ascii -le 25))) {$Ascii = $Ascii + (26-$Shift)}
+      else {$Ascii = $Ascii - $Shift}
+    }
+    $TextResult += [char]$Ascii
+  }
+  return $TextResult
+}
