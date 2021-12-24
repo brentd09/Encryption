29c80aa9213335791af70a444a6ee744febf6b6b
Brent Denny <brent.denny@ddls.com.au>
2021-12-24 21:44:30 +1000

    new caesar cipher

---
+++ 0.1/CaesarCipher.psm1
@@ -0,0 +1,39 @@
+﻿function ConvertTo-CaesarCipher {
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Text,
+    [int]$Shift = 1
+  )
+  $CipherResult = ''
+  $Chars = $Text.ToCharArray()
+  foreach ($Char in $Chars) {
+    $Ascii = [byte][char]$Char
+    if ($Char -match '[a-z]') {
+      if ($Ascii -eq 122 -or $Ascii -eq 90) {$Ascii = $Ascii -25}
+      else {$Ascii++}
+    }
+    $CipherResult += [char]$Ascii
+  }
+  return $CipherResult
+}
+
+function ConvertFrom-CaesarCipher {
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Cipher,
+    [int]$Shift = 1
+  )
+  $TextResult = ''
+  $Chars = $Cipher.ToCharArray()
+  foreach ($Char in $Chars) {
+    $Ascii = [byte][char]$Char
+    if ($Char -match '[a-z]') {
+      if ($Ascii -eq 97 -or $Ascii -eq 65) {$Ascii = $Ascii +25}
+      else {$Ascii--}
+    }
+    $TextResult += [char]$Ascii
+  }
+  return $TextResult
+}
